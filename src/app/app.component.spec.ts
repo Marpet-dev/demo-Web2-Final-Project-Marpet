81f03fd19725ada66707242adf23041156296e2f
Phan Quang Minh Long <60604069+minhlong26052001@users.noreply.github.com>
2022-05-17 17:24:30 +0700

    update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Web2-Final-Project-MarPet'`, () => {
+  it(`should have as title 'Web2-Final-Project-MarPet-Official'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Web2-Final-Project-MarPet');
+    expect(app.title).toEqual('Web2-Final-Project-MarPet-Official');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Web2-Final-Project-MarPet app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Web2-Final-Project-MarPet-Official app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Web2-Final-Project-MarPet';
+  title = 'Web2-Final-Project-MarPet-Official';
 }
